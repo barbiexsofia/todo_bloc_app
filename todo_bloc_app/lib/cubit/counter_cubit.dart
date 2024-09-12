21c7d121b83b2d80be11e5466912f879f466bfbc
Barbara Ilardo <ilardobarbarasofia@gmail.com>
2024-09-12 14:47:29 +0200

    tutorial minute 23:50

---
+++ todo_bloc_app/lib/cubit/counter_cubit.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CounterCubit extends Cubit<int> {
+  CounterCubit() : super(0);
+
+  void increment() {
+    print("before: $state");
+    emit(state + 1);
+    print("after: $state");
+  }
+
+  void decrement() {
+    print("before: $state");
+
+    if (state == 0) {
+      return;
+    }
+    emit(state - 1);
+    print("after: $state");
+  }
+}
