7b1c403703b9f437c95fe78a743b523b2a174db8
Barbara Ilardo <ilardobarbarasofia@gmail.com>
2024-09-13 16:30:27 +0200

    Tutorial minute 49:00 - Substitution of Cubit logic with Bloc; Implementation of MultiBlocProvider in main.dart.

--- todo_bloc_app/lib/inc_dec_page.dart
+++ todo_bloc_app/lib/inc_dec_page.dart
@@ -1,24 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:todo_bloc_app/cubit/counter_cubit.dart';
+import 'package:todo_bloc_app/bloc/counter_bloc.dart';
 
 class IncDecPage extends StatelessWidget {
   const IncDecPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final counterCubit = BlocProvider.of<CounterCubit>(context);
+    final counterBloc = BlocProvider.of<CounterBloc>(context);
     return Scaffold(
         floatingActionButton: Column(
       mainAxisAlignment: MainAxisAlignment.end,
       children: [
         FloatingActionButton(
-          onPressed: counterCubit.increment,
+          onPressed: () {
+            counterBloc.add(CounterIncremented());
+          },
           tooltip: 'Increment',
           child: const Icon(Icons.add),
         ),
         FloatingActionButton(
-          onPressed: counterCubit.decrement,
+          onPressed: () {
+            counterBloc.add(CounterDecremented());
+          },
           tooltip: 'Decrement',
           child: const Icon(Icons.minimize),
         ),
--- todo_bloc_app/lib/main.dart
+++ todo_bloc_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_bloc_app/bloc/counter_bloc.dart';
 import 'package:todo_bloc_app/cubit/counter_cubit.dart';
 import 'package:todo_bloc_app/home_page.dart';
 
@@ -12,8 +13,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (_) => CounterCubit(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => CounterCubit()),
+        BlocProvider(create: (_) => CounterBloc()),
+      ],
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
